f0efd9d091d2ad44413e9108283c119aa14d3cbc
ybhargava08 <32349135+ybhargava08@users.noreply.github.com>
2018-05-15 18:21:57 -0400

    Update Suggestions.java

--- src/main/java/com/yb/hitdb/model/Suggestions.java
+++ src/main/java/com/yb/hitdb/model/Suggestions.java
@@ -1,18 +1,84 @@
-package com.yb.hitdb.repo;
+package com.yb.hitdb.model;
 
-import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+import org.springframework.context.annotation.Primary;
 
-import com.yb.hitdb.model.Suggestions;
+@Entity
+@Table(name="Suggestions")
+public class Suggestions {
 
-@Repository
-public interface SuggestionsRepo extends CrudRepository<Suggestions, String>{
-	      
-	      @Query("select new com.yb.hitdb.model.Suggestions(env,hostname,username,password,serviceId) "
-	      		+ "from Suggestions where LOWER(env) like LOWER(CONCAT(:key,'%'))")
-          public List<Suggestions> findAllByKey(@Param("key") String key);
+	private String env;
+	private String hostname;
+	private String username;
+	private String password;
+	private String serviceId;
+	
+	public Suggestions(String env,String hostname,String username,String password,String serviceId) {
+		this.env = env;
+		this.hostname = hostname;
+		this.username = username;
+		this.password = password;
+		this.serviceId = serviceId;
+	}
+	
+	public Suggestions() {}
+
+	@Primary
+	@Column
+	@Id
+	public String getEnv() {
+		return env;
+	}
+	public void setEnv(String env) {
+		this.env = env;
+	}
+	
+	@Column
+	public String getHostname() {
+		return hostname;
+	}
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+	
+	@Column
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	@Column
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public int hashCode() {
+		return this.env.hashCode();
+	}
+	
+	public boolean equals(Object o) {
+		return ((Suggestions)o).getEnv().equalsIgnoreCase(this.env);
+	}
+	
+	public String toString() {
+		return this.env;
+	}
+	
+	@Column
+	public String getServiceId() {
+		return serviceId;
+	}
+	public void setServiceId(String serviceId) {
+		this.serviceId = serviceId;
+	}
+	
 }

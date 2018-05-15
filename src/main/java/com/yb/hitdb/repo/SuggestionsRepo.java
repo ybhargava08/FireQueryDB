a06f814b995372d0399711b5fc3d6ffa968e3249
ybhargava08 <32349135+ybhargava08@users.noreply.github.com>
2018-05-15 18:22:50 -0400

    Update SuggestionsRepo.java

--- src/main/java/com/yb/hitdb/repo/SuggestionsRepo.java
+++ src/main/java/com/yb/hitdb/repo/SuggestionsRepo.java
@@ -1,5 +1,18 @@
 package com.yb.hitdb.repo;
 
-public interface SuggestionsRepo {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.yb.hitdb.model.Suggestions;
+
+@Repository
+public interface SuggestionsRepo extends CrudRepository<Suggestions, String>{
+	      
+	      @Query("select new com.yb.hitdb.model.Suggestions(env,hostname,username,password,serviceId) "
+	      		+ "from Suggestions where LOWER(env) like LOWER(CONCAT(:key,'%'))")
+          public List<Suggestions> findAllByKey(@Param("key") String key);
 }

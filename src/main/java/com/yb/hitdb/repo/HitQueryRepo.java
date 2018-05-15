de5d832aa4e51afa38b0f01127a0a9e97c9bfa69
ybhargava08 <32349135+ybhargava08@users.noreply.github.com>
2018-05-15 18:22:26 -0400

    Update HitQueryRepo.java

--- src/main/java/com/yb/hitdb/repo/HitQueryRepo.java
+++ src/main/java/com/yb/hitdb/repo/HitQueryRepo.java
@@ -1,5 +1,35 @@
 package com.yb.hitdb.repo;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class HitQueryRepo {
 
+	public String runQuery(String query,JdbcTemplate template) {
+		String result = null;
+		try {
+			int rows = template.update(query);
+			result = createResult(query,rows);
+		}catch(Exception ex) {
+			result = "Error "+ex.getMessage();
+		}
+		return result;
+	}
+	
+	private String createResult(String query,int rows) {
+		 String result = String.valueOf(rows);
+		if(query.toLowerCase().replaceAll("\\s+", "").startsWith("update")) {
+			result+=" Updated";
+		}else if(query.toLowerCase().replaceAll("\\s+", "").startsWith("insert")) {
+			result+=" Inserted";
+		}else if(query.toLowerCase().replaceAll("\\s+", "").startsWith("delete")) {
+			result+=" Deleted";
+		}else {
+			result+=" Affected";
+		}
+		return result;
+	}
+	
+	
 }
